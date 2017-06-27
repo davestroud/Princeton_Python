ff0762ed574a17c9d649c0de0ea19b1d6d9768bd
David Stroud <david@greymatter-analytics.com>
2017-06-27 17:11:47 -0500

    fix comments in windchill.py

--- introcs-1.0/windchill.py
+++ introcs-1.0/windchill.py
@@ -1,16 +1,16 @@
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # windchill.py
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 import stdio
 import sys
 
- # Accept as command-line arguments a temperature t (in Fahrenheit)
- # and a wind speed v (in miles per hour). Compute the wind chill
- # w using the formula from the National Weather Service.
- # Write w to standard output.
- #
- #  Reference:  http://www.nws.noaa.gov/om/windchill/index.shtml
+""" Accept as command-line arguments a temperature t (in Fahrenheit)
+ and a wind speed v (in miles per hour). Compute the wind chill
+ using the formula from the National Weather Service.
+ Write w to standard output.
+
+ Reference:  http://www.nws.noaa.gov/om/windchill/index.shtml """
 
 t = float(sys.argv[1])
 v = float(sys.argv[2])
