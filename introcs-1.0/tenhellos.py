2639f2f84c3bb82c698abc6d0520135bc3b826d6
David Stroud <david@greymatter-analytics.com>
2017-07-02 18:02:28 -0500

    add tenhellos

---
+++ introcs-1.0/tenhellos.py
@@ -0,0 +1,16 @@
+# -----------------------------------------------------------------------
+# tenhellos.py
+# -----------------------------------------------------------------------
+
+import stdio
+
+# Write 10 Hellos to standard output
+
+stdio.writeln('1st Hello')
+stdio.writeln('2nd Hello')
+stdio.writeln('3rd Hello')
+
+i = 4
+while i <= 10:
+    stdio.writeln(str(i) + 'th Hello')
+    i = i + 1
